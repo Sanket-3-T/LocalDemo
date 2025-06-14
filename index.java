f93483ad0a13c670f98833b2d468d5882068c2a1
Sanket-3-T <sanketkharche507@gmail.com>
2025-06-14 11:23:58 +0530

    inital file added

---
+++ index.java
@@ -0,0 +1,15 @@
+package LocalDemo;
+
+import java.util.*;
+
+public class index {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = new String();
+        System.out.println("Enter the name : ");
+        name = sc.nextLine();
+        System.out.println("" + name);
+        sc.close();
+
+    }
+}
